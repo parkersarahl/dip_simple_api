84eb1a1c510c3f3499f94131775d6c29ecc5b249
Sarah Parker <sarahlparker4@gmail.com>
2024-07-03 08:45:00 -0400

    added wait command for both instances changed app added build

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, attempting to deploy a change';
+    return 'Hello World, we have liftoff...and a database';
   }
 }
